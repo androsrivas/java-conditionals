9410a049c766a564b15e8f269fc4a945f7fcd1d9
dustisme <androsrivas@outlook.com>
2024-11-19 11:21:03 +0100

    correct traffic light exercise

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -1,27 +1,30 @@
 public class TrafficLights {
     public static void main(String[] args) {
-        
-        String trafficLight = "";
-        
-        if (trafficLight == "green") {
-            System.out.println("Puedes seguir");
-        } else if (trafficLight == "amber") {
-            System.out.println("Ten cuidado");
-        } else if (trafficLight == "red") {
-            System.out.println("¡¡¡Para!!!");
-        } else {
-            System.out.println("El semáforo está dañado");
-        }
         //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo
         //determine las acciones de un conductor
-        
+
         //Si es verde imprime en la terminal: Puedes seguir
         //Si es amarillo imprime en la terminal: Ten cuidado
         //Si es rojo imprime en la terminal: ¡¡¡Para!!!
         //Si es otro color imprime en la terminal: El semáforo está dañado
 
-        //Cambia el valor de la variable, corre el programa y mira el resultado 
+        //Cambia el valor de la variable, corre el programa y mira el resultado
 
+        String trafficLight = "";
+
+        switch(trafficLight) {
+            case "green":
+                System.out.println("Puedes seguir");
+                break;
+            case "amber":
+                System.out.println("Ten cuidado");
+                break;
+            case "red":
+                System.out.println("¡¡¡Para!!!");
+                break;
+            default:
+                System.out.println("El semáforo está dañado");
+        }
     }
     
 }
