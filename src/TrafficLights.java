9e1b58a5e6e2490c7d5dcbffee1d082650d8bfa7
dustisme <androsrivas@outlook.com>
2024-11-19 10:50:46 +0100

    finish traffic lights exercise

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -1,7 +1,19 @@
 public class TrafficLights {
     public static void main(String[] args) {
-
-        //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
+        
+        String trafficLight = "";
+        
+        if (trafficLight == "green") {
+            System.out.println("Puedes seguir");
+        } else if (trafficLight == "amber") {
+            System.out.println("Ten cuidado");
+        } else if (trafficLight == "red") {
+            System.out.println("¡¡¡Para!!!");
+        } else {
+            System.out.println("El semáforo está dañado");
+        }
+        //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo
+        //determine las acciones de un conductor
         
         //Si es verde imprime en la terminal: Puedes seguir
         //Si es amarillo imprime en la terminal: Ten cuidado
